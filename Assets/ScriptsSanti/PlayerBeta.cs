7db027a992b9ab7dc2f74d4a400d05b1b311a887
Bruno Giacometti <152458112+chaskibum@users.noreply.github.com>
2025-08-11 13:04:58 -0300

    Feedback power up mejorado
    ahora aparece un "aura" alrededor del player al agarrar el power up, que indica que es invulnerable

--- Assets/ScriptsSanti/PlayerBeta.cs
+++ Assets/ScriptsSanti/PlayerBeta.cs
@@ -14,6 +14,7 @@ public class PlayerBeta : MonoBehaviour
     [SerializeField] Transform playerVisuals;
     [SerializeField] GameObject velocityParticles;
     [SerializeField] GameObject PowerupVisual;
+    [SerializeField] GameObject PowerupVisual2;
     [SerializeField] Animator CameraAnimator;
     CircleCollider2D _hitbox;
     Vector2 startPos;
@@ -198,6 +199,7 @@ public class PlayerBeta : MonoBehaviour
                 }
 
                 PowerupVisual.SetActive(true);
+                PowerupVisual2.SetActive(true);
 
                 itemsManager.ChangeItemsSpeed(5, PowerUpDuration);
 
@@ -267,6 +269,7 @@ public class PlayerBeta : MonoBehaviour
         yield return new WaitForSeconds(PowerUpDuration);
         StopCoroutine("ActivatePowerupWarning");
         PowerupVisual.SetActive(false);
+        PowerupVisual2.SetActive(false);
         isInvincible = false;
         DisablePowerUpCoroutine = null;
         AudioManager.Instance.motorbikeSound.pitch -= 0.5f;
